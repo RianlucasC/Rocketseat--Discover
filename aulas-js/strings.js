d29dc5e9ba43b636dafa597c79d5892a495bf4b7
Rianlucas7 <109886025+RianlucasC@users.noreply.github.com>
2023-01-14 12:30:33 -0300

    Create strings.js

---
+++ aulas-js/strings.js
@@ -0,0 +1,5 @@
+console.log("O rato 'roeu' a roupa do rei de Roma") // aspas duplas 
+
+console.log('O rato "roeu" a roupa do rei de Roma') // aspas simples 
+
+console.log(`O "rato" 'roeu' a roupa do rei de Roma ${1 + 1} vezes`) // template literals 
