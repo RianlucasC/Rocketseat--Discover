34cff0930cd11062a2f25716643bfbec19afd62c
Rianlucas7 <109886025+RianlucasC@users.noreply.github.com>
2023-01-15 14:23:24 -0300

    att

---
+++ aulas-js/array.js
@@ -0,0 +1,14 @@
+const animals = [
+    "lion",
+    "monkey",
+    "cat",
+    {
+        name: "dog",
+        age: 3,
+    }
+]
+
+console.log(animals[1]);
+console.log(animals.length)
+console.log(animals[3])
+console.log(animals[3].age)
---
+++ exercicios/exercicioJS/script.js
@@ -0,0 +1,19 @@
+let weight;
+console.log(typeof weight);
+
+let name = "clebi";
+let age = 17;
+let stars = 4.9;
+let isSubscribed = true;
+
+let student = {
+    name: "clebi",
+    age: 17,
+    weight: 64.9,
+    isSubscribed: true,
+};
+console.log(student);
+
+console.log(`${student.name} de idade ${student.age} pesa ${student.weight} quilos`)
+
+let students = [];
