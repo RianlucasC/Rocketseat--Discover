c860736d111b1b327ab6921b9812ef65ea17b68b
Rianlucas7 <109886025+RianlucasC@users.noreply.github.com>
2023-01-16 12:10:41 -0300

    Update functions_2.js

---
+++ aulas-js/functions_2.js
@@ -0,0 +1,10 @@
+const sum = function(num1,num2) {
+    return num1 + num2;
+}
+
+let number1 = 34;
+let number2 = 25;
+
+console.log(`o número 1 é ${number1}`)
+console.log(`o número 2 é ${number2}`)
+console.log(`a soma é ${sum(number1, number2)}`)
