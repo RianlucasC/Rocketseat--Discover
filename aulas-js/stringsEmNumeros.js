85da8a5e5c1db075a304c808678fce18f4f96e5f
Rianlucas7 <109886025+RianlucasC@users.noreply.github.com>
2023-01-17 15:17:09 -0300

    Create stringsEmNumeros.js

---
+++ aulas-js/stringsEmNumeros.js
@@ -0,0 +1,7 @@
+let string = "128";
+let transformaNumber = Number(string);
+console.log(typeof transformaNumber);
+
+let number = 46;
+let transformaString = String(number);
+console.log(typeof transformaString)
