4f82c09c1f656ffb09f0bd9c0fc70b0f7844de1d
Rianlucas7 <109886025+RianlucasC@users.noreply.github.com>
2023-01-18 11:19:11 -0300

    Update arrays2.js

--- aulas-js/arrays2.js
+++ aulas-js/arrays2.js
@@ -1,7 +1,9 @@
 let techs = ["html", "css", "js"];
 techs.push("nodejs"); // adiciona ao final 
 techs.unshift("sql"); // adiciona no começo
+let copy = techs.slice(0,2) // copia do array
 console.log(techs);
 techs.pop(); // remove do final
 techs.shift(); // remove do inicio
 console.log(techs);
+console.log(copy);
