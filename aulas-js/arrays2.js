19e94e3649eed7619c6231bc9b4cfea6bccadb30
Rianlucas7 <109886025+RianlucasC@users.noreply.github.com>
2023-01-18 02:56:23 -0300

    metodos array

---
+++ aulas-js/arrays2.js
@@ -0,0 +1,7 @@
+let techs = ["html", "css", "js"];
+techs.push("nodejs"); // adiciona ao final 
+techs.unshift("sql"); // adiciona no começo
+console.log(techs);
+techs.pop(); // remove do final
+techs.shift(); // remove do inicio
+console.log(techs);
---
+++ aulas-js/includes.js
@@ -0,0 +1,2 @@
+let myString = "O rato roeu a roupa do rei de Roma";
+console.log(myString.includes("rato"));
