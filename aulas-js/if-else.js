9d093cb29b6c7c38aa5a88185704134b9b0c3beb
Rianlucas7 <109886025+RianlucasC@users.noreply.github.com>
2023-01-19 02:54:52 -0300

    switch

---
+++ aulas-js/if-else.js
@@ -0,0 +1,7 @@
+let temperatura = 35;
+
+if (temperatura >= 37) {
+    console.log("febre");
+} else {
+    console.log("saudável");
+}
---
+++ aulas-js/switch.js
@@ -0,0 +1,19 @@
+let expression = "b";
+switch (expression) { // expressão
+    case "a":
+        // se a expressão for "a" execute :
+        console.log("a")
+        break
+    case "b":
+        // se a expressão for "b" execute :
+        console.log("b")
+        break
+    case "c":
+        // se a expressão for "c" execute :
+        console.log("c")
+        break
+    default :
+        // se a expressão não for nenhuma das anteriores execute :
+        console.log("defaut");
+        break
+}
