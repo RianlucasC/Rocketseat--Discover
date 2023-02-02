9bf80de1dcee8c9bc357892d33a5dd556cedb08b
Rianlucas7 <109886025+RianlucasC@users.noreply.github.com>
2023-02-02 14:23:20 -0300

    a

--- exercicios/exercicioJS/Celsius-to-Fahrenheit.js
+++ exercicios/exercicioJS/Celsius-to-Fahrenheit.js
@@ -1,8 +1,15 @@
 function CelsiusToFahrenheit (temp) {
-    temp.toUpperCase();
-    if (temp.includes("C")) {
-        
+    let num;
+    if (temp.includes("C") || temp.includes("c")) {
+        num = Number(temp.toUpperCase().replace("C", ""));
+        return `${num * 9/5 + 32}F`;
+    } else if (temp.includes("F") || temp.includes("f")) {
+        num = Number(temp.toUpperCase().replace("F", ""));
+        return `${(num - 32) * 5/9}C`;
     } else {
-
+        console.log("Grau não identificado");
     }
 }
+
+console.log(CelsiusToFahrenheit("10C"));
+console.log(CelsiusToFahrenheit("50F"));
