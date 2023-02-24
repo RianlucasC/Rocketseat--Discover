609de630708ea92070e92ce3d4ed63396dfc7c4b
Rianlucas7 <109886025+RianlucasC@users.noreply.github.com>
2023-02-23 21:08:48 -0300

    Update clearTimeout.js

--- aulas-node/clearTimeout.js
+++ aulas-node/clearTimeout.js
@@ -1,7 +1,7 @@
-// clear timeOut vai canselar um setTimeout
+// clear timeOut vai cancelar um setTimeout
 
 let timer = setTimeout(() => {
-    console.log("função executada")
+    console.log("função executada");
 }, 3000);
 
 clearTimeout(timer); // cancelando o timer 
