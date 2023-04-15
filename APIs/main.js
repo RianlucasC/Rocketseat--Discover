72ccc300219f9d259b87bedf9edf1afbb49a68aa
Rianlucas7 <rianlucas592@gmail.com>
2023-04-15 16:35:43 -0300

    fetch

--- APIs/main.js
+++ APIs/main.js
@@ -3,8 +3,8 @@ const url = "http://localhost:5500/api";
 function getUsers() {
     fetch(url)
     .then(response => response.json())
-    .then()
+    .then(data => renderApiResult.textContent = JSON.stringify(data))
     .catch(error => console.error(error))
-}
+};
 
 getUsers();
