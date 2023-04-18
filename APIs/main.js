aff281898e82a74e15b9be73f30f238e6baf3628
Rianlucas7 <rianlucas592@gmail.com>
2023-04-18 20:57:24 -0300

    delete com fetch

--- APIs/main.js
+++ APIs/main.js
@@ -44,6 +44,18 @@ function updateUser(id, updateUser) {
     .catch( error => console.error(error))
 }
 
+function deleteUser(id) {
+    fetch(`${url}/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then(response => response.json())
+    .then(data => alertApi.textContent = data)
+    .catch(error => console.error(error))
+}
+
 const updatedUser = {
     name: "Marcelão",
     avatar: "http://picsum.photos/200/300",
@@ -56,7 +68,10 @@ const newUser = {
     city: "Minas gerais"
 }
 
-updateUser(1,updatedUser);
-//addUser(newUser);
-getUsers();
+
 getUser("1");
+//updateUser(1,updatedUser);
+//addUser(newUser);
+//deleteUser("1")
+getUsers();
+
