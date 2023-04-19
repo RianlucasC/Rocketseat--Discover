28a7e5e7253b5606b1917d9771f0176125cbc0e5
Rianlucas7 <rianlucas592@gmail.com>
2023-04-19 11:21:12 -0300

    post com axios

--- APIs/usando-axios/script.js
+++ APIs/usando-axios/script.js
@@ -1,4 +1,3 @@
-
 const url = "http://localhost:5500/api";
 
 function getUsers() {
@@ -9,4 +8,30 @@ function getUsers() {
     .catch(error => console.error(error))
 }
 
+function addNewUser(newUser) {
+    axios.post(url, newUser)
+    .then(response => console.log(response))
+    .catch(error => console.error(error))
+}
+
+function getUser(id) {
+    axios.get(`${url}/${id}`)
+    .then(response => {
+        const data = response.data;
+        userName.textContent = data.name;
+        userCity.textContent = data.city;
+        userId.textContent = data.id;
+        userAvatar.src = data.avatar;
+    })
+    .catch(error => console.error(error))
+}
+
+const newUser = {
+    name: "Rian",
+    avatar: "https://picsum.photos/200/300",
+    city: "são paulo"
+};
+
+//addNewUser(newUser);
 getUsers();
+getUser("1");
